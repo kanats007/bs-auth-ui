06ff9e4f1273ea0bd5e2cc872bba6f3963758766
kanats <kanats007m@gmail.com>
2024-03-08 11:20:07 +0900

    作業実績ページの追加

---
+++ src/app/(authenticated)/work-result/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default async function Logined() {
+  return (
+    <div>
+      <h1>作業実績ページ</h1>
+      <Link href="/">トップページへ</Link>
+    </div>
+  );
+}
